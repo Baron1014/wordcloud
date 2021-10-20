829654496a2fd7d70d76258eb2c2ca9bdfff9cf2
Baron <nm6101080@gs.ncku.edu.tw>
2021-10-20 14:56:53 +0800

    add barplot

--- data.py
+++ data.py
@@ -110,3 +110,86 @@ def get_media_data():
     return pd.DataFrame({
         "col": dflist
         })
+
+def get_scene_data():
+    # 8-1. 課程情境氛圍營造
+    dflist = list()
+    dflist.extend(['課程初始引起動機' for _ in range(10)])
+    dflist.extend(['營造與課程主題或教學目標相關的課程氛圍' for _ in range(8)])
+
+    return pd.DataFrame({
+        "col": dflist
+        })
+
+def get_motivation_data():
+    # 8-2. 學生缺乏學習動機
+    dflist = list()
+    dflist.extend(['了解原因' for _ in range(10)])
+    dflist.extend(['與導師或輔導老師討論思考因應方式' for _ in range(8)])
+    dflist.extend(['調整及反思課程設計與教學方式' for _ in range(8)])
+
+    return pd.DataFrame({
+        "col": dflist
+        })
+
+def get_interrupt_data():
+    # 8-3. 學生課堂干擾行為
+    dflist = list()
+    dflist.extend(['操作制約' for _ in range(7)])
+    dflist.extend(['調整' for _ in range(10)])
+    dflist.extend(['了解原因' for _ in range(10)])
+    dflist.extend(['反思課程設計與教學方式' for _ in range(8)])
+    dflist.extend(['與導師或輔導老師討論思考因應方式' for _ in range(8)])
+
+    return pd.DataFrame({
+        "col": dflist
+        })
+
+def get_alienated_data():
+    # 8-4. 學生人際疏離行為
+    dflist = list()
+    dflist.extend(['彈性調整分組方式' for _ in range(7)])
+    dflist.extend(['設計疏離行為的議題融入課程' for _ in range(10)])
+    dflist.extend(['了解原因' for _ in range(10)])
+    dflist.extend(['與導師或輔導老師討論思考因應方式' for _ in range(8)])
+
+    return pd.DataFrame({
+        "col": dflist
+        })
+
+def get_disappoint_data():
+    # 8-5. 學生情緒失落
+    dflist = list()
+    dflist.extend(['了解學生情緒失落的原因' for _ in range(10)])
+    dflist.extend(['設計情緒調節課程' for _ in range(6)])
+    dflist.extend(['與導師或輔導老師討論思考因應方式' for _ in range(8)])
+
+    return pd.DataFrame({
+        "col": dflist
+        })
+
+def get_special_data():
+    # 8-6. 特殊學生
+    dflist = list()
+    dflist.extend(['事先了解班級學生的狀況' for _ in range(10)])
+    dflist.extend(['擬定相關教學策略' for _ in range(9)])
+    dflist.extend(['與導師或輔導老師或特教老師進行討論' for _ in range(10)])
+    dflist.extend(['設計相關課程' for _ in range(7)])
+    dflist.extend(['教育班級其他學生與特殊生的相處方式' for _ in range(10)])
+
+    return pd.DataFrame({
+        "col": dflist
+        })
+
+def get_conflict_data():
+    # 8-7. 學生課堂衝突行為
+    dflist = list()
+    dflist.extend(['立即制止並分開衝突雙方' for _ in range(10)])
+    dflist.extend(['注意學生安全與受教權益' for _ in range(6)])
+    dflist.extend(['依照班級狀況判斷處理方式' for _ in range(8)])
+    dflist.extend(['了解衝突原因' for _ in range(10)])
+    dflist.extend(['通知並與導師討論學生的衝突行為與處理方式' for _ in range(10)])
+
+    return pd.DataFrame({
+        "col": dflist
+        })
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import data
 import numpy as np
 import collections
+import seaborn as sns
 from PIL import Image
 from wordcloud import WordCloud
+from matplotlib.font_manager import FontProperties
+myfont=FontProperties(fname=r'C:\Windows\Fonts\msjh.ttc',size=14)
+sns.set(font=myfont.get_family())
+sns.set_style("whitegrid",{"font.sans-serif":['Microsoft JhengHei']})
 
 def wc(d, figname):
     mask = np.array(Image.open("cloud.png"))
@@ -26,6 +32,19 @@ def wc(d, figname):
     plt.imshow(wc)
     plt.axis("off")
     wc.to_file(f"report/{figname}.jpg")
+
+def catbarplot(d, title):
+    stick_name = [i for i in pd.unique(d['col'])]
+
+    plt.figure(figsize=(10,8))
+    sns.countplot(x='col', data=d, palette="flare")
+    plt.xticks(range(len(stick_name)) ,[re.sub("(.{10})", "\\1\n", label, 0, re.DOTALL) for label in stick_name])
+    plt.title(title)
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(f'./report/{title}.jpg')
+    plt.close()
+    
 
 def main():
     coaching = data.get_coaching_data()
@@ -57,5 +76,29 @@ def main():
     d = data.get_media_data()
     wc(d['col'], "媒體知識")
 
+def barplot():
+    # 8.情景知識
+    d8 = data.get_scene_data()
+    catbarplot(d8, "課程情境氛圍營造")
+
+    d8 = data.get_motivation_data()
+    catbarplot(d8, "學生缺乏學習動機")
+
+    d8 = data.get_interrupt_data()
+    catbarplot(d8, "學生課堂干擾行為")
+
+    d8 = data.get_alienated_data()
+    catbarplot(d8, "學生人際疏離行為")
+
+    d8 = data.get_disappoint_data()
+    catbarplot(d8, "學生情緒失落")
+
+    d8 = data.get_special_data()
+    catbarplot(d8, "特殊學生")
+
+    d8 = data.get_conflict_data()
+    catbarplot(d8, "學生課堂衝突行為")
+
 if __name__ == "__main__":
-    main()
+    # main()
+    barplot()
